61ebac877c0bd11cfcbed8a7132147c8a185660e
Ryan Santos <ryan.santos@teradata.com>
2018-06-12 14:09:15 -0700

    Create class for e1050 switch

--- common/src/stack/switch/command/list/switch/mac/imp_e1050.py
+++ common/src/stack/switch/command/list/switch/mac/imp_e1050.py
@@ -4,41 +4,30 @@
 # https://github.com/Teradata/stacki/blob/master/LICENSE.txt
 # @copyright@
 
-from itertools import groupby
-import json
 import re
-import requests
 import stack.commands
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-
-def natural_sort(s):
-	return [int(''.join(g)) if k else ''.join(g) for k, g in groupby('\0' + s, str.isdigit)]
-
-
-def net_show_interface(imp, switch):
-	switch_name = switch['host']
-	url = f'https://{switch["ip"]}:8080/nclu/v1/rpc'
-	payload = {"cmd": "show interface json"}
-	auth = (imp.owner.getHostAttr(switch_name, 'switch_username'),
-	        imp.owner.getHostAttr(switch_name, 'switch_password'))
-	headers = {'Content-Type': 'application/json'}
-
-	data = json.loads(requests.post(url, headers=headers, json=payload, auth=auth, verify=False).text)
-	for iface in sorted(data, key=natural_sort):
-		port_match = re.search(r'\d+', iface)
-		if port_match is not None and iface not in ['bridge.1', 'eth0']:
-			info = data[iface]['iface_obj']
-
-			port = port_match.group()
-			vlan = info['vlan'][0]['vlan']
-
-			imp.owner.addOutput(switch_name, [port, info['mac'], '', iface, vlan])
+from stack.switch import SwitchCelesticaE1050  #, SwitchException
 
 
 class Implementation(stack.commands.Implementation):
 	def run(self, args):
 		switch = args[0]
-		net_show_interface(self, switch)
+
+		switch_name = switch['host']
+		switch_address = switch['ip']
+		switch_username = self.owner.getHostAttr(switch_name, 'switch_username')
+		switch_password = self.owner.getHostAttr(switch_name, 'switch_password')
+
+		with SwitchCelesticaE1050(switch_address, switch_name, switch_username, switch_password) as switch:
+			with switch.sorted_json("show interface json") as data:
+				for iface in data:
+					port_match = re.search(r'\d+', iface)
+					info = data[iface]
+					if 'swp' in iface and info['linkstate'] != 'DN':
+						iface_obj = info['iface_obj']
+
+						port = port_match.group()
+						vlan = '' if not iface_obj['vlan'] else iface_obj['vlan'][0]['vlan']  # handle multiple VLANs?
+
+						self.owner.addOutput(switch_name,
+						                    [port, iface_obj['mac'], '', iface, vlan])  # host missing, switch hostfile?
