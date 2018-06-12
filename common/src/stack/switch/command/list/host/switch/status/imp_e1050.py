a585643d6364932ca93c3632d27eac5add177e49
Ryan Santos <ryan.santos@teradata.com>
2018-06-12 14:12:13 -0700

    Add e1050 impl. of 'stack list host switch status'

---
+++ common/src/stack/switch/command/list/host/switch/status/imp_e1050.py
@@ -0,0 +1,47 @@
+# @copyright@
+# Copyright (c) 2006 - 2018 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+
+from itertools import groupby
+import json
+import re
+import requests
+import stack.commands
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def natural_sort(s):
+	return [int(''.join(g)) if k else ''.join(g) for k, g in groupby('\0' + s, str.isdigit)]
+
+
+def blah(imp, switch):
+	hostnames = imp.owner.hosts
+
+	switch_name = switch['host']
+	url = f'https://{switch["ip"]}:8080/nclu/v1/rpc'
+	payload = {"cmd": "show interface json"}
+	auth = (imp.owner.getHostAttr(switch_name, 'switch_username'),
+	        imp.owner.getHostAttr(switch_name, 'switch_password'))
+	headers = {'Content-Type': 'application/json'}
+
+	data = json.loads(requests.post(url, headers=headers, json=payload, auth=auth, verify=False).text)
+	for iface in sorted(data, key=natural_sort):
+		port_match = re.search(r'\d+', iface)
+		info = data[iface]
+		if 'swp' in iface:  # and host (switch hostfile) in hostnames
+			iface_obj = info['iface_obj']
+
+			port = port_match.group()
+			vlan = '' if not iface_obj['vlan'] else iface_obj['vlan'][0]['vlan']  # handle multiple VLANs?
+
+			imp.owner.addOutput('?', [iface_obj['mac'], iface, vlan, switch_name, port, info['speed'], info['linkstate']])  # host missing, switch hostfile?
+
+
+class Implementation(stack.commands.Implementation):
+	def run(self, args):
+		switch = args[0]
+		blah(self, switch)
