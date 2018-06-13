67b713b59936b801991bfc170ce74ad0be250334
Ryan Santos <ryan.santos@teradata.com>
2018-06-13 12:00:55 -0700

    Rearrange e1050 switch class

--- common/src/stack/switch/pylib/switch/e1050.py
+++ common/src/stack/switch/pylib/switch/e1050.py
@@ -7,13 +7,14 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
+# might be unused
+def natural_sort(s):
+	return [int(''.join(g)) if k else ''.join(g) for k, g in groupby('\0' + s, str.isdigit)]
+
+
 class SwitchCelesticaE1050(Switch):
 	"""Class for interfacing with a Celestica e1050 switch running Cumulus Linux.
 	"""
-
-	@staticmethod  # needed?
-	def natural_sort(s):
-		return [int(''.join(g)) if k else ''.join(g) for k, g in groupby('\0' + s, str.isdigit)]
 
 	def rpc_req_text(self, cmd):
 		url = f'https://{self.switch_ip_address}:8080/nclu/v1/rpc'
@@ -23,8 +24,10 @@ class SwitchCelesticaE1050(Switch):
 
 		return requests.post(url, headers=headers, json=payload, auth=auth, verify=False).text
 
-	def sorted_keys(self, dct):
-		return sorted(dct, key=self.natural_sort)
+	# might be unused
+	@staticmethod
+	def sorted_keys(dct):
+		return sorted(dct, key=natural_sort)
 
 	def json_loads(self, cmd):
 		return json.loads(self.rpc_req_text(cmd))
