0ed83a9af62be204b04a37c5b51f833b4e0a7e6d
Ryan Santos <ryan.santos@teradata.com>
2018-06-13 11:58:24 -0700

    Use host MAC and name

--- common/src/stack/switch/command/list/switch/mac/imp_e1050.py
+++ common/src/stack/switch/command/list/switch/mac/imp_e1050.py
@@ -19,18 +19,20 @@ class Implementation(stack.commands.Implementation):
 		switch_username = self.owner.getHostAttr(switch_name, 'switch_username')
 		switch_password = self.owner.getHostAttr(switch_name, 'switch_password')
 
+		# better to get hosts from switch hostfile? also, 'net show bridge macs <ip>' suggests switch knows host IP
+		hosts = self.owner.call('list.host.interface', ['output-format=json'])
 		with SwitchCelesticaE1050(switch_address, switch_name, switch_username, switch_password) as switch:
-			data = switch.json_loads("show interface json")
-			for iface in switch.sorted_keys(data):
-				port_match = re.search(r'\d+', iface)
-				info = data[iface]
-				if 'swp' in iface and info['linkstate'] != 'DN':
-					iface_obj = info['iface_obj']
+			# better name(s)?
+			for iface_obj in sorted(switch.json_loads(cmd="show bridge macs dynamic json"), key=lambda d: d['dev']):  # why did they call iface 'dev'?
+				mac = iface_obj['mac']
+				port = re.search(r'\d+', iface_obj['dev']).group()
+				vlan = iface_obj['vlan']  # should VLAN come from FE or switch? Missing from FE atm
+				# multiple VLANs?
 
-					port = port_match.group()
-					# mac and interface are for host, but are stored in switch; figure out where
-					# should vlan come from FE or switch?
-					vlan = '' if not iface_obj['vlan'] else iface_obj['vlan'][0]['vlan']  # handle multiple VLANs?
+				for host_obj in hosts:
+					if host_obj['mac'] == mac:
+						host = host_obj['host']
+						interface = host_obj['interface']
 
-					self.owner.addOutput(switch_name, [port, iface_obj['mac'], '', iface, vlan])  # host missing, switch hostfile?
-
+						self.owner.addOutput(switch_name, [port, mac, host, interface, vlan])
+						break
