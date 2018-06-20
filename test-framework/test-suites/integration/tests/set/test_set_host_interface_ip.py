717d2eea42f458c2f1037d7b73f0a9edc990157a
Aish <cyberaishu@gmail.com>
2018-06-20 13:09:13 -0700

    Use set_host_interface test fixture instead of duplicating code.

--- test-framework/test-suites/integration/tests/set/test_set_host_interface_ip.py
+++ test-framework/test-suites/integration/tests/set/test_set_host_interface_ip.py
@@ -1,37 +1,20 @@
 import json
 import os
+import pytest
 import subprocess
 
-import pytest
-
-
 @pytest.mark.usefixtures("add_host")
+@pytest.mark.usefixtures("set_host_interface")
 class TestSetHostInterfaceIp:
 	def test_set_host_interface_ip_auto(self, host):
-		"Test the output when the discovery daemon is not running"
+		"Test if ip=AUTO assigns a valid, unique IP address"
+		
+		hostname  = set_host_interface['host']
+		interface = set_host_interface['interface']
+		ip_list   = set_host_interface['ip_list']
 
-		# Make sure discovery isn't running
-		op = host.run("stack list network private output-format=json")
-		o = json.loads(op.stdout)
-		addr = o[0]['address']
-		mask = o[0]['mask']
-
-		op = host.run("stack list host a:backend output-format=json")
-		o = json.loads(op.stdout)
-		hostname = o[0]['host']
-
-		op = host.run("stack list host interface output-format=json")
-		o = json.loads(op.stdout)
-		ip_list = []
-		interface = None
-
-		for line in o:
-			if line['host'] == hostname:
-				interface = line['interface']
-			# Make list of IP addresses
-			ip_list.append(line['ip'])
-
-		op = host.run("stack set host interface ip %s ip=AUTO interface=%s" % (hostname, interface))
+		op = host.run("stack set host interface ip %s ip=AUTO interface=%s" % \
+			(hostname, interface))
 		assert op.rc == 0
 		
 		op = host.run("stack list host interface %s output-format=json" % hostname)
